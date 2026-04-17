fb115fb53dacd3b2c16f9b86842703353d0a5728
marisloading <mar.pendao@hotmail.com>
2026-04-17 12:41:10 +0100

    Finish exercise 9 of foundations successfully

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  let para = [min, max];
+  let sum = 0;
 
+  if (para.some((num) => num < 0 || !Number.isInteger(num))) {
+    return "ERROR";
+  } else {
+    if (min > max) {
+      [min, max] = [max, min];
+    }
+    for (i = max; i > min - 1; i--) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
