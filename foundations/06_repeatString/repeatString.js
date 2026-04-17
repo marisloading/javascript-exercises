8fa118fcd733d0784e7b44014aa890a65a20a05f
marisloading <mar.pendao@hotmail.com>
2026-04-17 11:50:24 +0100

    Finished exercise 6 of foundations successfully

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let multiString = "";
 
+  if (num < 0) {
+    multiString = "ERROR";
+  } else {
+    for (let i = 0; i < num; i++) {
+      multiString += string;
+    }
+  }
+
+  return multiString;
 };
 
 // Do not edit below this line
